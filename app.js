2d30740cdf874c4a38da5a85441d78ff036e9280
Buntu William <williambuntu1@gmail.com>
2023-03-28 23:44:09 +0200

    2nd commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add')
 const list = document.querySelector('.todos')
+const search = document.querySelector('search input')
 
 const generaTemplet = todo =>{
   const html = ` <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -14,6 +15,33 @@ addForm.addEventListener('submit', e => {
     e.preventDefault()
 
     const todo = addForm.add.value.trim()
-    
+    if (todo.length){
     generaTemplet(todo)
+    addForm.reset()
+    }
+})
+
+// deleting t todos 
+
+list.addEventListener('click',(e)=>{
+
+  if( e.target.classList.contains('delete')){
+    e.target.parentElement.remove()
+  }
+})
+
+const filtreTodo = (term) =>{
+  Array.from(list.children).forEach(todo => {
+    if (todo.textContent.includes(term)) {
+      todo.classList.remove('filtered')
+    } else {
+      todo.classList.add('filtered')
+    }
+  })
+}
+
+
+search.addEventListener('keyup',()=>{
+ const term = search.value.trim()
+ filtreTodo(term)
 })
