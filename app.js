4881d5d1dd5445a63ce527479b70711c47f4a43c
Buntu William <williambuntu1@gmail.com>
2023-03-28 10:41:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const addForm = document.querySelector('.add')
+const list = document.querySelector('.todos')
+
+const generaTemplet = todo =>{
+  const html = ` <li class="list-group-item d-flex justify-content-between align-items-center">
+  <span>${todo}</span>
+  <i class="far fa-trash-alt delete"></i>
+</li>
+  `
+  list.innerHTML += html
+}
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const todo = addForm.add.value.trim()
+    
+    generaTemplet(todo)
+})
